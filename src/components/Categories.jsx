404914dfc361cf8a91a472099299b960c4d41d5b
ostandrey <ost.and.g@gmail.com>
2023-04-25 12:36:19 +0300

    init: create project

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">All</li>
+        <li>Meat</li>
+        <li>Vegetarian</li>
+        <li>Grill</li>
+        <li>Spicy</li>
+        <li>Covered</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
